cc25225c6152bd367eccc37271bbc739d48b6bb5
eeplater <eeplater@central.uh.edu>
2022-03-02 13:48:49 -0600

    added pytorch-lightning configurations to simplify code and add more customizations. Still need to implement DataLoader on pl model

--- siamese_net.py
+++ siamese_net.py
@@ -2,7 +2,6 @@ import torchvision
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader,Dataset
-import matplotlib.pyplot as plt
 import torchvision.utils
 import numpy as np
 import random
@@ -13,10 +12,74 @@ import PIL.ImageOps
 import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
+from pytorch_lightning.core.lightning import LightningModule
+from pytorch_lightning.callbacks import ModelCheckpoint
+from scipy.stats import multivariate_normal
+
+def get_gaussian_mask():
+	#128 is image size
+	# We will be using 256x128 patch instead of original 128x128 path because we are using for pedestrain with 1:2 AR.
+	x, y = np.mgrid[0:1.0:256j, 0:1.0:128j] #128 is input size.
+	xy = np.column_stack([x.flat, y.flat])
+	mu = np.array([0.5,0.5])
+	sigma = np.array([0.22,0.22])
+	covariance = np.diag(sigma**2) 
+	z = multivariate_normal.pdf(xy, mean=mu, cov=covariance) 
+	z = z.reshape(x.shape) 
+
+	z = z / z.max()
+	z  = z.astype(np.float32)
+
+	mask = torch.from_numpy(z)
+
+	return mask
+
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        self.eps = 1e-9
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        losses = 0.5 * (label.float() * euclidean_distance + (1 + (-1 * label) ).float() * F.relu(self.margin - (euclidean_distance + self.eps).sqrt()).pow(2))
+        loss_contrastive = torch.mean(losses)
+
+        return loss_contrastive
+
+
+class TripletLoss(nn.Module):
+    """
+    Triplet loss
+    Takes embeddings of an anchor sample, a positive sample and a negative sample
+    """
+
+    def __init__(self, margin):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, anchor, positive, negative, size_average=True):
+        distance_positive = F.cosine_similarity(anchor,positive) #Each is batch X 512 
+        distance_negative = F.cosine_similarity(anchor,negative)  # .pow(.5)
+        losses = (1- distance_positive)**2 + (0 - distance_negative)**2      #Margin not used in cosine case. 
+        return losses.mean() if size_average else losses.sum()
 
 
 
-class SiameseNetwork(nn.Module):
+
+criterion = TripletLoss(margin=1)
+
+#Multiply each image with mask to give attention to center of the image.
+gaussian_mask = get_gaussian_mask().cuda()
+
+
+class SiameseNetwork(LightningModule):
     def __init__(self):
         super(SiameseNetwork, self).__init__()
 
@@ -78,39 +141,24 @@ class SiameseNetwork(nn.Module):
 
         return output1, output2
 
+    def training_step(self, batch, batch_idx):
+		
+        # Get anchor, positive and negative samples
+        anchor, positive, negative = batch
+        anchor, positive, negative = anchor.cuda(), positive.cuda() , negative.cuda()
+ 
+ 		# Multiple image patches with gaussian mask. It will act as an attention mechanism which will focus on the center of the patch
+        anchor, positive, negative = anchor*gaussian_mask, positive*gaussian_mask, negative*gaussian_mask
+
+        anchor_out, positive_out, negative_out = self(anchor, positive, negative) # Model forward propagation
+
+        triplet_loss = criterion(anchor_out, positive_out, negative_out) # Compute triplet loss (based on cosine simality) on the output feature maps
+        self.log("triplet_loss", triplet_loss.item(), logger = True, on_step = True, on_epoch = False)
+        return triplet_loss
 
 
-class ContrastiveLoss(torch.nn.Module):
-    """
-    Contrastive loss function.
-    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-    """
-
-    def __init__(self, margin=2.0):
-        super(ContrastiveLoss, self).__init__()
-        self.margin = margin
-        self.eps = 1e-9
-
-    def forward(self, output1, output2, label):
-        euclidean_distance = F.pairwise_distance(output1, output2)
-        losses = 0.5 * (label.float() * euclidean_distance + (1 + (-1 * label) ).float() * F.relu(self.margin - (euclidean_distance + self.eps).sqrt()).pow(2))
-        loss_contrastive = torch.mean(losses)
-
-        return loss_contrastive
+    def configure_optimizer(self):
+        optim.Adam(self.parameters(), lr = 0.0005)
 
 
-class TripletLoss(nn.Module):
-    """
-    Triplet loss
-    Takes embeddings of an anchor sample, a positive sample and a negative sample
-    """
 
-    def __init__(self, margin):
-        super(TripletLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, anchor, positive, negative, size_average=True):
-        distance_positive = F.cosine_similarity(anchor,positive) #Each is batch X 512 
-        distance_negative = F.cosine_similarity(anchor,negative)  # .pow(.5)
-        losses = (1- distance_positive)**2 + (0 - distance_negative)**2      #Margin not used in cosine case. 
-        return losses.mean() if size_average else losses.sum()
--- siamese_train.py
+++ siamese_train.py
@@ -1,26 +1,12 @@
-import torchvision
 import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader,Dataset
-import matplotlib.pyplot as plt
-import torchvision.utils
-import numpy as np
-import random
-from PIL import Image
-import torch
-from torch.autograd import Variable
+from torchvision import transforms
+from torch.utils.data import DataLoader
 import PIL.ImageOps    
-import torch.nn as nn
-from torch import optim
-import torch.nn.functional as F
-
 from siamese_dataloader import *
 from siamese_net import *
-
-import nonechucks as nc
-from scipy.stats import multivariate_normal
-import os
 from tqdm import tqdm
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 """
 Get training data
@@ -30,84 +16,46 @@ class Config():
 	training_dir = "/media/ADAS1/MARS/bbox_train/bbox_train/"
 	testing_dir = "/media/ADAS1/MARS/bbox_test/bbox_test/"
 	train_batch_size = 128
-	train_number_epochs = 100	
+
 
 folder_dataset = dset.ImageFolder(root=Config.training_dir)
 
-transforms = torchvision.transforms.Compose([
-	torchvision.transforms.Resize((256,128)),
-	torchvision.transforms.ColorJitter(hue=.05, saturation=.05),
-	torchvision.transforms.RandomHorizontalFlip(),
-	torchvision.transforms.RandomRotation(20, resample=PIL.Image.BILINEAR),
-	torchvision.transforms.ToTensor()
+transforms = transforms.Compose([
+	transforms.Resize((256,128)),
+	transforms.ColorJitter(hue=.05, saturation=.05),
+	transforms.RandomHorizontalFlip(),
+	transforms.RandomRotation(20, resample=PIL.Image.BILINEAR),
+	transforms.ToTensor()
 	])
 
 
-def get_gaussian_mask():
-	#128 is image size
-	# We will be using 256x128 patch instead of original 128x128 path because we are using for pedestrain with 1:2 AR.
-	x, y = np.mgrid[0:1.0:256j, 0:1.0:128j] #128 is input size.
-	xy = np.column_stack([x.flat, y.flat])
-	mu = np.array([0.5,0.5])
-	sigma = np.array([0.22,0.22])
-	covariance = np.diag(sigma**2) 
-	z = multivariate_normal.pdf(xy, mean=mu, cov=covariance) 
-	z = z.reshape(x.shape) 
 
-	z = z / z.max()
-	z  = z.astype(np.float32)
-
-	mask = torch.from_numpy(z)
-
-	return mask
 
 
 
 siamese_dataset = SiameseTriplet(imageFolderDataset=folder_dataset,transform=transforms,should_invert=False) # Get dataparser class object
-net = SiameseNetwork().cuda() # Get model class object and put the model on GPU
-
-criterion = TripletLoss(margin=1)
-optimizer = optim.Adam(net.parameters(),lr = 0.0005 ) #changed from 0.0005
-
-print("GPU compute available: ", torch.cuda.is_available())
-
-counter = []
-loss_history = [] 
-iteration_number= 0
-
 train_dataloader = DataLoader(siamese_dataset,shuffle=True,num_workers=14,batch_size=Config.train_batch_size) # PyTorch data parser obeject
 
-#Multiply each image with mask to give attention to center of the image.
-gaussian_mask = get_gaussian_mask().cuda()
 
-for epoch in range(0,Config.train_number_epochs):
-	for i, data in enumerate(train_dataloader,0):
+net = SiameseNetwork().cuda() # Get model class object and put the model on GPU
 
-		# Get anchor, positive and negative samples
-		anchor, positive, negative = data
-		anchor, positive, negative = anchor.cuda(), positive.cuda() , negative.cuda()
- 
- 		# Multiple image patches with gaussian mask. It will act as an attention mechanism which will focus on the center of the patch
-		anchor, positive, negative = anchor*gaussian_mask, positive*gaussian_mask, negative*gaussian_mask
 
-		optimizer.zero_grad() # Reset the optimizer gradients to zero
 
-		anchor_out, positive_out, negative_out = net(anchor, positive, negative) # Model forward propagation
 
-		triplet_loss = criterion(anchor_out, positive_out, negative_out) # Compute triplet loss (based on cosine simality) on the output feature maps
-		triplet_loss.backward() # Backward propagation to get the gradients w.r.t. model weights
-		optimizer.step() # Model weights updation using calculated gradient and Adam optimizer
+# saves a file like: my/path/sample-mnist-epoch=02-val_loss=0.32.ckpt
+checkpoint_callback = ModelCheckpoint(
+    every_n_epochs = 20,
+    dirpath = "lightning_logs/model_ckpts",
+    filename="Deep-SORT-{epoch:02d}-{val_loss:.2f}"
+)
 
-		if i %10 == 0 : # Print logs after every 10 iterations
-			#TODO: Update with tqdm based log printing for better training monitoring
-			print("Epoch number {}\n Current loss {}\n".format(epoch,triplet_loss.item()))
-			iteration_number +=10
-			counter.append(iteration_number)
-			loss_history.append(triplet_loss.item())
-	if epoch%20==0: # Model will be saved after every 20 epochs
-		if not os.path.exists('ckpts/'):
-			os.mkdir('ckpts')
-		torch.save(net,'ckpts/model'+str(epoch)+'.pt')
 
-# Loss curve plot to be dumped after full model training. 
-show_plot(counter,loss_history,path='ckpts/loss.png')
+
+
+trainer = Trainer(log_every_n_steps=10,
+                  max_epochs = 100,
+                  gpus = -1,
+                  callbacks=[checkpoint_callback],
+				  profiler = "advanced") 
+
+trainer.fit(net, train_dataloader)
