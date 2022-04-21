4778e564fea07034d71d85a1776d8b8592286f18
eeplater <eeplater@central.uh.edu>
2022-04-21 17:58:50 -0500

    modified keyon's cropper file to generalize it better

--- MOT to Market-1501 Converter Tool/cropper.py
+++ MOT to Market-1501 Converter Tool/cropper.py
@@ -1,48 +1,54 @@
+from jsonargparse import ArgumentParser, ActionConfigFile
 from io import TextIOWrapper
 from PIL import Image
+from pathlib import Path
 import sys
 import os
 
-def crop(bounds: TextIOWrapper, video, framesPath, destPath):
-    for line in bounds:
-        args = line.split(',')
-        frameNum = args[0]
-        id = args[1]
-        x = int(args[2])
-        y = int(args[3])
-        width = int(args[4])
-        height = int(args[5])
-        idPath = os.path.join(destPath, video + "_" + id)
-        if (not os.path.exists(idPath)):
-            os.mkdir(idPath)
-        frame = Image.open(os.path.join(framesPath, frameNum.zfill(6) + ".jpg"))
-        croppedImg = frame.crop((x, y, x + width, y + height))
-        croppedImg = croppedImg.save(os.path.join(idPath, video + "_" + frameNum + "_" + id + ".jpg"))
-        
-        
+def crop(fname: str, framesPath: str, destPath: str):
+    with open(fname, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            args = line.split(',')
+            frameNum = args[0]
+            id = args[1].zfill(3)
+            x = int(args[2])
+            y = int(args[3])
+            width = int(args[4])
+            height = int(args[5])
+            idPath = os.path.join(destPath, id)
+            if (not os.path.exists(idPath)):
+                os.mkdir(idPath)
+            frame = Image.open(os.path.join(framesPath, frameNum.zfill(6) + ".jpg"))
+            croppedImg = frame.crop((x, y, x + width, y + height))
+            out_img = os.path.join(idPath, frameNum + "_" + id + ".jpg")
+            croppedImg.save(out_img)
 
 
-def main():
-    if os.path.exists(os.path.join(os.getcwd(), sys.argv[1])):
-        directory = os.path.join(os.getcwd(), sys.argv[1])
-    elif os.path.exists(sys.argv[1]):
-        directory = sys.argv[1]
+def main(mot_path: str, is_det: bool):
+    mot_path = Path(mot_path)
+    if is_det:
+        mot_files = mot_path.rglob("det.txt")
+        cropped_dir_name = "crops_det"
     else:
-        print("Invalid Path")
-        return -1
-
-    cropsPath = os.path.join(directory, "crops")
-    if (not os.path.exists(cropsPath)):
-        os.mkdir(cropsPath)
-        os.mkdir(os.path.join(cropsPath, "gt"))
-    for video in os.listdir(directory):
-        if (video == "crops"):
-            continue
-        videoPath = os.path.join(directory, video)
-        framesPath = os.path.join(videoPath, "img1")
-        if (os.path.exists(os.path.join(videoPath, "gt"))):
-            crop(open(os.path.join(videoPath, "gt/gt.txt")), video, framesPath, os.path.join(cropsPath, "gt"))
+        mot_files = mot_path.rglob("gt.txt")
+        cropped_dir_name = "crops_gt"
+    
+    for mot_file in mot_files:
+        video_path = mot_file.parent.parent
+        frames_path = video_path / "img1"
+        cropped_path = video_path / cropped_dir_name
+        cropped_path.mkdir(exist_ok = True)
+        crop(mot_file, frames_path, cropped_path)
         
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--mot_path", type=str, required=True, help="path to MOT directory in local machine")
+    parser.add_argument("--is_det", type=bool, required=False, help="do we use bboxes in `det.txt`?")
+    parser.add_argument("--config", action = ActionConfigFile)
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args.mot_path, args.is_det)
